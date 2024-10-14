29a63ce18a667ad1fabf0b6b146d4584be6fc5c1
ASGodolfim <stonegodolfim@gmail.com>
2024-10-14 11:01:32 -0300

    feat config.env

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const morgan = require('morgan');
 
-const tourRouter = require(`./routes/tourRoutes`);
-const userRouter = require(`./routes/userRoutes`);
+const tourRouter = require(`${__dirname}/routes/tourRoutes`);
+const userRouter = require(`${__dirname}/routes/userRoutes`);
 
 const app = express();
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public/`));
