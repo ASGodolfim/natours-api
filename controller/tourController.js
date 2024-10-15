9439cbb9c998c4640f82c5d4c1f5f5454c1f06bf
ASGodolfim <stonegodolfim@gmail.com>
2024-10-15 13:34:28 -0300

    refactor apifeatures

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,8 +1,9 @@
 const Tour = require('./../models/tourModel');
+const features = require('./../utils/apiFeatures');
 
 exports.getAllTours = async (req, res) => {
 try{
-    const features = new APIFeature(Tour.find(), req.query).filter().sort().limit().pagination();
+    const features = new APIFeature(Tour.find(), req.query).filter().sort().limitFields().pagination();
     const tours = await features.query;
 
     res.status(200).json({
@@ -82,60 +83,6 @@ exports.deleteTour = async (req, res) => {
             status: 'fail',
             message: 'Not Found'
         })
-    }
-};
-
-class APIFeature{
-    constructor(query, queryString){
-        this.query = query;
-        this.queryString = queryString;
-    }
-
-    filter(){
-        const queryObj = {...this.queryString};
-        const excludedFields = ['page', 'sort', 'limit', 'fields'];
-        excludedFields.forEach(element => {
-            delete queryObj[element]
-        });
-
-        let queryStr = JSON.stringfy(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, match => `$${match}`);
-    
-        this.query = this.query.find(JSON.parse(queryStr));
-
-        return this;
-    }
-    sort(){
-
-        if(this.queryString.sort){
-            const sortBy = this.queryString.sort.split(',').join(' ');
-            this.query = this.query.sort(sortBy);
-        } else {
-            this.query = this.query.sort('-createdAt');
-        }
-
-        return this;
-    }
-
-    limit(){
-        if (this.queryString.fields) {
-            const fields = this.queryString.fields.split(',').join(' ');
-            this.query = this.query.select(fields);
-        } else {
-            this.query = this.query.select('-__v');
-        }
-        
-        return this;
-    }
-
-    pagination(){
-        const page = this.queryString.page * 1 || 1;
-        const limit = this.queryString.limit *1 || 100;
-        const skip = (page - 1) * limit;
-        
-        this.query = this.query.skip(skip).limit(limit)
-
-        return this;
     }
 };
 
