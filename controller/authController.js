73b7b9d98e565c8d9f9c43f3a6c082c74810f091
ASGodolfim <stonegodolfim@gmail.com>
2024-10-16 17:23:21 -0300

    feat jwt

--- controller/authController.js
+++ controller/authController.js
@@ -1,12 +1,24 @@
 const catchAsync = require('../utils/catchAsync');
-const User = require('./../models/userModel')
+const User = require('./../models/userModel');
+const jwt = require('jsonwebtoken');
 
 exports.signup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+        name: req.body.name,
+        email: req.body.email,
+        age: req.body.age,
+        photo: req.body.photo
+    });
+
+    const token = jwt.sign({ id: newUser._id}, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN});
 
     res.status(201).json(
         {
             status: 'success',
+            token,
             data: {
                 user: newUser
             }
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -44,6 +45,14 @@ const userSchema = new mongoose.Schema({
         }
     }
 );
+
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return next();
+
+    this.password = await bcryptjs.hash(this.password, 12);
+    this.passwordConfirm = undefined;
+    next();
+})
 
 const User = mongoose.model('User', userSchema);
 
