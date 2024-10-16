36cb02317274cc157e9d3fa3d0901672337b227e
ASGodolfim <stonegodolfim@gmail.com>
2024-10-16 15:13:14 -0300

    feat error handler server

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ dotenv.config({path: './config.env'});
 
 const app = require('./app');
 
+
+
 mongoose.connect(process.env.DATABASE_LOCAL).then(con =>{
     console.log('DB Connected');
 });
@@ -11,4 +13,18 @@ mongoose.connect(process.env.DATABASE_LOCAL).then(con =>{
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message)
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message)
+    server.close(() => {
+        process.exit(1);
+    });
 });
