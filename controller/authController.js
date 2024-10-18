064a8b9e5a8a979d6fd83bf946886281b4f2117a
ASGodolfim <stonegodolfim@gmail.com>
2024-10-18 16:04:16 -0300

    token to cookies

--- controller/authController.js
+++ controller/authController.js
@@ -8,6 +8,14 @@ const sendEmail = require('./../utils/email');
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
+
+    const cookieOptions = { 
+        expires: new Date(Date.now() + process.env.JWT_COOKIES_EXPIRES_IN * 24 * 60 * 1000),
+        httpOnly: true
+    }
+    if(process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+    res.cookie('jwt', token, cookieOptions); 
+
     res.status(statusCode).json(
         {
             status: 'success',
