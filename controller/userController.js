43533d5c86ccb3464852708610dcfa52280c59bd
ASGodolfim <stonegodolfim@gmail.com>
2024-10-18 15:17:33 -0300

    feat update me

--- controller/userController.js
+++ controller/userController.js
@@ -3,6 +3,14 @@ const APIFeature = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError')
 
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach(el => {
+        if (allowedFields.includes(el)) newObj[el] = obj[el];
+    });
+    return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
 
     const features = new APIFeature(user.find(), req.query).filter().sort().limitFields().pagination();
@@ -52,6 +60,26 @@ exports.updateUser = catchAsync(async (req, res, next) => {
         }
     );
 });
+exports.updateMe = catchAsync(async (req, res, next) => {
+    if (req.body.password || req.body.passwordConfirm){
+        return next(new AppError('please use update password for updating the password'), 400);
+    }
+    const filteredBody = filterObj(req.body, 'name', 'email', 'username')
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {new: true, runValidators: true})
+
+    res.status(200).json(
+        {
+            status: 'succsess',
+            data: {
+                user
+            }
+        }
+    )
+
+
+
+
+});
 exports.deleteUser = catchAsync(async (req, res, next) => {
     const user = await User.findByIdAndDelete(req.params.id);
 
@@ -66,6 +94,8 @@ exports.deleteUser = catchAsync(async (req, res, next) => {
         }
     );
 });
+
+
 /*
 exports.createUser = catchAsync(async (req, res, next) => {
     const newUser = await User.create(req.body);
