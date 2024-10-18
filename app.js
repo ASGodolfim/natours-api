f859bb711a95b5fdcf7a1277965053479177fcf7
ASGodolfim <stonegodolfim@gmail.com>
2024-10-18 16:32:30 -0300

    feat helmet

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ const morgan = require('morgan');
 const AppError = require('./utils/appError');
 const errorHandler = require('./controller/errorHandler');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const tourRouter = require(`${__dirname}/routes/tourRoutes`);
 const userRouter = require(`${__dirname}/routes/userRoutes`);
 
 const app = express();
-
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
-};
 
 const limiter = rateLimit({
     max: 100,
@@ -19,7 +16,16 @@ const limiter = rateLimit({
     message: 'too many requests from this IP, please try again in an hour'
 });
 
+
+app.use(helmet())
+
 app.use('/api', limiter);
+
+app.use(express.json( { limit: '10kb' } ));
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+};
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public/`));
