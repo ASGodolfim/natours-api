7d99e2f4318190e811cedc1f083cc0a32cb5e1b2
ASGodolfim <stonegodolfim@gmail.com>
2024-10-18 16:17:33 -0300

    limiter

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const AppError = require('./utils/appError');
-const errorHandler = require('./controller/errorHandler')
+const errorHandler = require('./controller/errorHandler');
+const rateLimit = require('express-rate-limit');
 
 const tourRouter = require(`${__dirname}/routes/tourRoutes`);
 const userRouter = require(`${__dirname}/routes/userRoutes`);
@@ -10,7 +11,15 @@ const app = express();
 
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
-}
+};
+
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'too many requests from this IP, please try again in an hour'
+});
+
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public/`));
