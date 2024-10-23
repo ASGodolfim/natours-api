94e0ae2b5c91e1a315596f46ab5da956752da545
ASGodolfim <stonegodolfim@gmail.com>
2024-10-23 17:04:31 -0300

    feat popup on map marker

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -5,8 +5,7 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoiam9uYXNzY2htZWR0bWFubiIsImEiOiJjam54ZmM5N3gwN
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/jonasschmedtmann/cjvi9q8jd04mi1cpgmg7ev3dy',
-    center: [-118.113491, 34.111745],
-    zoom: 4
+    scrollZoom: false
 });
 
 const bounds = new mapboxgl.LngLatBounds();
@@ -20,12 +19,20 @@ locations.forEach(loc => {
         anchor: 'bottom'
     }).setLngLat(loc.coordinates).addTo(map);
 
+    new mapboxgl.Popup({
+        offset: 30
+    })
+        .setLngLat(loc.coordinates)
+        .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+        .addTo(map);
+
     bounds.extend(loc.coordinates)
+
 });
 
 map.fitBounds(bounds, {
     top: 200,
-    bottom: 200,
+    bottom: 150,
     left: 100,
     right: 100
 });
